d54873b6b8a2773570ab9a0f521af21560e0f628
mona <mona-rea.nerva@voco.ee>
2024-04-03 10:46:02 +0300

    Kasutaja saab sisestada hinda ja kogust ning naeb kogu hinda ilma maksudeta

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-console.log("Hello world");
+const readline = require('readline');
 
+// Create interface for reading from stdin
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+let hind;
+let kogus;
+//hind
+rl.question('Palun sisesta toote hind: ', (input) => {
+    hind = input;
+  console.log('Sisestatud hind:', input);
+  //kogus
+  rl.question('Palun sisesta toote kogus: ', (input) => {
+    kogus = input;
+    console.log('Sisestatud kogus:', input);    
+    rl.close();
+    // inputi lopp
+    console.log("---------------")
+    // hind maksudeta
+    ilmaMaksuta = hind * kogus
+    console.log("Hind maksudeta: ", ilmaMaksuta)
+  });
+});
